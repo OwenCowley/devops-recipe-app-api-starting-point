13584efbb6eca479ef133ed46e2220cd26843a6c
ocowley1 <ocowley1@gmail.com>
2025-02-23 18:27:06 +0000

    added terraform

---
+++ infra/deploy/main.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.23.0"
+    }
+  }
+
+  backend "s3" {
+    bucket               = "devops-recipe-app-tf-state-owen"
+    key                  = "tf-state-deploy"
+    workspace_key_prefix = "tf-state-deploy-env"
+    region               = "eu-west-1"
+    encrypt              = true
+    dynamodb_table       = "devops-recipe-app-tf-state-lock-owen"
+  }
+}
+
+provider "aws" {
+  region = "eu-west-1"
+  default_tags {
+    tags = {
+      Environment = terraform.workspace
+      Project     = var.project
+      contact     = var.contact
+      ManagedBy   = "Terraform/deploy"
+    }
+  }
+}
+
+locals {
+  prefix = "${var.prefix}-${terraform.workspace}"
+}
+
+data "aws_region" "current" {}
---
+++ infra/setup/main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.23.0"
+    }
+  }
+  backend "s3" {
+    bucket         = "devops-recipe-app-tf-state-owen"
+    key            = "tf-state-setup"
+    region         = "eu-west-1"
+    encrypt        = true
+    dynamodb_table = "devops-recipe-app-api-lock"
+  }
+}
+
+provider "aws" {
+  region = "eu-west-1"
+  default_tags {
+    tags = {
+      Environment = terraform.workspace
+      Project     = var.project
+      contact     = var.contact
+      ManagedBy   = "Terraform/setup"
+    }
+  }
+}
