1f4f39cbcb2b28acaf4ff43c41e1d320829ce146
ocowley1 <ocowley1@gmail.com>
2025-03-18 08:34:00 +0000

    fix incorrect

--- infra/deploy/network.tf
+++ infra/deploy/network.tf
@@ -27,7 +27,7 @@ resource "aws_internet_gateway" "main" {
 resource "aws_subnet" "public_a" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.1.1.0/24"
-  map_customer_owned_ip_on_launch = true
+  map_public_ip_on_launch = true
   availability_zone = "${data.aws_region.current.name}a"
 
   tags = {
@@ -57,7 +57,7 @@ resource "aws_route" "public_internet_access_a" {
 resource "aws_subnet" "public_b" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.1.2.0/24"
-  map_customer_owned_ip_on_launch = true
+  map_public_ip_on_launch = true
   availability_zone = "${data.aws_region.current.name}b"
 
   tags = {
