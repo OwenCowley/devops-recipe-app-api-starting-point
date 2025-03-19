2d8d6934b5e118232227e39d1f5c65e2a76385b7
ocowley1 <ocowley1@gmail.com>
2025-03-19 08:44:56 +0000

    more linting

--- infra/deploy/network.tf
+++ infra/deploy/network.tf
@@ -113,15 +113,15 @@ resource "aws_subnet" "private_b" {
 #########################################################################
 
 resource "aws_security_group" "endpoint_access" {
-  description = "Access to endpoints"
-  name = "${local.prefix}-endpoint-access"
-  vpc_id = aws_vpc.main.id
+  description   = "Access to endpoints"
+  name          = "${local.prefix}-endpoint-access"
+  vpc_id        = aws_vpc.main.id
 
   ingress {
     cidr_blocks = [aws_vpc.main.cidr_block]
-    from_port = 443
-    to_port = 443
-    protocol = "tcp"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
   }
 }
 
